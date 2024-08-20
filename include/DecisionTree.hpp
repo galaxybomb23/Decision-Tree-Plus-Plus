8df7dbb112b1f3b35f637ba14eae7762d5080f42
Cooper Rockwell <cooper8r8@gmail.com>
2024-08-20 11:50:14 -0400

    starter files

---
+++ include/DecisionTree.hpp
@@ -0,0 +1,14 @@
+#ifndef DECISION_TREE_HPP
+#define DECISION_TREE_HPP
+
+class DecisionTree
+{
+public:
+    DecisionTree(); // Constructor
+    ~DecisionTree(); // Destructor
+
+private:
+    // Private members
+};
+
+#endif // DECISION_TREE_HPP
---
+++ src/DecisionTree.cpp
@@ -0,0 +1,13 @@
+#include "DecisionTree.hpp"
+
+DecisionTree::DecisionTree()
+{
+
+}
+
+DecisionTree::~DecisionTree()
+{
+
+}
+
+// Other functions below
