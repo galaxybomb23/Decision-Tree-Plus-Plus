13ea2bbff2a2ea17e92c7a638bc837a186424767
Elijah Jorgensen <jorgenel@purdue.edu>
2024-08-21 11:03:05 -0400

    initial class variable and function definitions

--- src/DecisionTreeNode.cpp
+++ src/DecisionTreeNode.cpp
@@ -1,13 +1,23 @@
 #include "DecisionTreeNode.hpp"
 
-DecisionTreeNode::DecisionTreeNode()
+DecisionTreeNode::DecisionTreeNode(const std::string &feature, double entropy,
+                                   const std::vector<double> &class_probabilities,
+                                   const std::vector<string> &branch_features_and_values_or_thresholds,
+                                   DecisionTree &dt, const bool isRoot)
+    : _dt(dt), _feature(feature), _nodeCreationEntropy(entropy),
+      _classProbabilities(class_probabilities),
+      _branchFeaturesAndValuesOrThresholds(branch_features_and_values_or_thresholds)
 {
-
+    if (isRoot)
+    {
+        dt.nodesCreated = -1;
+        dt.classNames.clear();
+    }
+    _serialNumber = GetNextSerialNum();
 }
 
 DecisionTreeNode::~DecisionTreeNode()
 {
-
 }
 
 // Other functions below
