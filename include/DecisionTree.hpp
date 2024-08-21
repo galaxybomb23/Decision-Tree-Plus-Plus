72e6fbd40e9de50b909b8aa5555a5b7671589009
Cooper Rockwell <cooper8r8@gmail.com>
2024-08-21 13:20:46 -0400

    updated and added some variables

--- include/DecisionTree.hpp
+++ include/DecisionTree.hpp
@@ -23,20 +23,31 @@ private:
     std::string _training_datafile;
     double _entropy_threshold;
     int _max_depth_desired;
+    int _number_of_histogram_bins;
     int _csv_class_column_index;
     int _symbolic_to_numeric_cardinality_threshold;
-    int _number_of_histogram_bins;
     int _csv_cleanup_needed;
     int _debug1, _debug2, _debug3;
+    int _how_many_total_training_samples;
+    int _nodes_created;
 
+    DecisionTreeNode* _root_node;
     std::vector<int> _csv_columns_for_features;
-    std::vector<std::string> _class_names;
+    std::map<std::string, double> _probability_cache;
+    std::map<std::string, double> _entropy_cache;
     std::map<std::string, std::vector<std::string>> _training_data_dict;
-    std::map<std::string, std::vector<double>> _numeric_features_valuerange_dict;
-    std::map<std::string, int> _feature_values_how_many_uniques_dict;
-    std::map<std::string, std::vector<std::string>> _features_and_values_dict;
+    std::map<std::string, std::set<std::string>> _features_and_values_dict;
     std::map<std::string, std::set<std::string>> _features_and_unique_values_dict;
     std::map<std::string, std::string> _samples_class_label_dict;
+    std::vector<std::string> _class_names;
+    std::map<std::string, double> _class_priors_dict;
+    std::vector<std::string> _feature_names;
+    std::map<std::string, std::vector<double>> _numeric_features_valuerange_dict;
+    std::map<std::string, std::vector<double>> _sampling_points_for_numeric_feature_dict;
+    std::map<std::string, int> _feature_values_how_many_uniques_dict;
+    std::map<std::string, std::vector<double>> _prob_distribution_numeric_features_dict;
+    std::map<std::string, double> _histogram_delta_dict;
+    std::map<std::string, int> _num_of_histogram_bins_dict;
 };
 
 #endif // DECISION_TREE_HPP
--- src/DecisionTree.cpp
+++ src/DecisionTree.cpp
@@ -27,12 +27,30 @@ DecisionTree::DecisionTree(std::map<std::string, std::string> kwargs)
         "debug3"
     };
 
-    // Initialize the class variables
+    // Set default values
     _entropy_threshold = 0.01;
     _symbolic_to_numeric_cardinality_threshold = 10;
-    _max_depth_desired = _csv_class_column_index = _number_of_histogram_bins = -1;
     _csv_cleanup_needed = 0;
+    _csv_columns_for_features = {};
     _debug1 = _debug2 = _debug3 = 0;
+    _max_depth_desired = _csv_class_column_index = _number_of_histogram_bins = -1;
+    _root_node = nullptr;
+    _how_many_total_training_samples = 0;
+    _probability_cache = {};
+    _entropy_cache = {};
+    _training_data_dict = {};
+    _features_and_values_dict = {};
+    _features_and_unique_values_dict = {};
+    _samples_class_label_dict = {};
+    _class_names = {};
+    _class_priors_dict = {};
+    _feature_names = {};
+    _numeric_features_valuerange_dict = {};
+    _sampling_points_for_numeric_feature_dict = {};
+    _feature_values_how_many_uniques_dict = {};
+    _prob_distribution_numeric_features_dict = {};
+    _histogram_delta_dict = {};
+    _num_of_histogram_bins_dict = {};
 
     // Check and set keyword arguments
     for (const auto& kv : kwargs) {
@@ -47,6 +65,10 @@ DecisionTree::DecisionTree(std::map<std::string, std::string> kwargs)
             _max_depth_desired = std::stoi(value);
         } else if (key == "csv_class_column_index") {
             _csv_class_column_index = std::stoi(value);
+        } else if (key == "csv_columns_for_features") {
+            for (const auto& c : value) {
+                _csv_columns_for_features.push_back(c);
+            }
         } else if (key == "symbolic_to_numeric_cardinality_threshold") {
             _symbolic_to_numeric_cardinality_threshold = std::stoi(value);
         } else if (key == "number_of_histogram_bins") {
