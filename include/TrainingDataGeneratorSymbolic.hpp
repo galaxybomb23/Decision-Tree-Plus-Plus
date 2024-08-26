c6fe4895dc72ee7b97a29a426bdb5201591ae770
Elijah Jorgensen <jorgenel@purdue.edu>
2024-08-26 13:12:16 -0400

    chore: Add new classes for training data evaluation and generation

---
+++ include/TrainingDataGeneratorSymbolic.hpp
@@ -0,0 +1,22 @@
+#ifndef TRAINING_DATA_GENERATOR_SYMBOLIC_HPP
+#define TRAINING_DATA_GENERATOR_SYMBOLIC_HPP
+
+class TrainingDataGeneratorSymbolic
+{
+private:
+    /* data */
+public:
+    TrainingDataGeneratorSymbolic(/* args */);
+    ~TrainingDataGeneratorSymbolic();
+
+    void ReadParameterFileSymbolic();    // Read the parameter file for symbolic data
+    void GenerateTrainingDataSymbolic(); // Generate the training data for symbolic data
+};
+
+TrainingDataGeneratorSymbolic::TrainingDataGeneratorSymbolic(/* args */)
+{
+}
+
+TrainingDataGeneratorSymbolic::~TrainingDataGeneratorSymbolic()
+{
+}
