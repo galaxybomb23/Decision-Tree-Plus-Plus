37d572e493be83daa6218b525e78204021791d3f
Tom_TKO <tkodonne@purdue.edu>
2024-08-27 10:01:29 -0400

    add test file, still fails tests though

--- test/DecisionTreeNodeTest.cpp
+++ test/DecisionTreeNodeTest.cpp
@@ -15,7 +15,19 @@ protected:
     }
 
     // Class members to be used in tests
-    std::map<std::string, std::string> kwargs;
+    std::map<std::string, std::string> kwargs = {
+        {"training_datafile", "resources/stage3cancer.csv"},
+        {"entropy_threshold", "0.01"},
+        {"max_depth_desired", "10"},
+        {"csv_class_column_index", "0"},
+        {"symbolic_to_numeric_cardinality_threshold", "10"},
+        {"csv_columns_for_features", "1,2,3,4,5,6,7,8,9,10"},
+        {"number_of_histogram_bins", "10"},
+        {"csv_cleanup_needed", "0"},
+        {"debug1", "0"},
+        {"debug2", "0"},
+        {"debug3", "0"}
+    };
     DecisionTree dt = DecisionTree(kwargs);
     DecisionTreeNode node = DecisionTreeNode("feature", 0.0, {0.0}, {"branch"}, dt, true);
 };
