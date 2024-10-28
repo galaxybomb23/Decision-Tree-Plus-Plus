982e34655c50a79a495a3debc751780403eb340d
Coop8 <cooper8r8@gmail.com>
2024-10-28 08:43:40 -0400

    cleaned up tests

--- test/ProbCalcTest.cpp
+++ test/ProbCalcTest.cpp
@@ -10,7 +10,7 @@ class ProbCalcTest : public ::testing::Test
     {
         // called before each test starts
         map<string,string> kwargs = {
-            {"training_datafile", "test/resources/training_symbol.csv"},
+            {"training_datafile", "../test/resources/training_symbolic.csv"},
             {"csv_class_column_index", "1"},
             {"csv_columns_for_features", "{2,3,4,5}"},
             {"max_depth_desired", "5"},
@@ -39,11 +39,11 @@ class ProbCalcTest : public ::testing::Test
 TEST_F(ProbCalcTest, CheckdtExists) { ASSERT_NE(&dt, nullptr); }
 
 TEST_F(ProbCalcTest, priorProbabilityForClass) {
-    double prob = dt.priorProbabilityForClass("benign");
-    ASSERT_EQ(prob, .62);
+    double prob = dt->priorProbabilityForClass("benign");
+    ASSERT_EQ(prob, 0.62);
 
-    prob = dt.priorProbabilityForClass("malignant");
-    ASSERT_EQ(prob,.38);
+    prob = dt->priorProbabilityForClass("malignant");
+    ASSERT_EQ(prob, 0.38);
 }
 
 TEST_F(ProbCalcTest, calculateClassPriors) {
@@ -51,7 +51,7 @@ TEST_F(ProbCalcTest, calculateClassPriors) {
 }
 
 TEST_F(ProbCalcTest, probabilityOfFeatureValue) {
-    auto prob = dt.probabilityOfFeatureValue("smoking", "heavy");
+    auto prob = dt->probabilityOfFeatureValue("smoking", "heavy");
     ASSERT_EQ(prob,.44);
     
 }
